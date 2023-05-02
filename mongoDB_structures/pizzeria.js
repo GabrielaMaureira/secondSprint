3e21d8e11ffc0e963730976f7d4e6d7c0854b503
Gabriela Maureira <ellamaureira@MacBook-Pro-de-Gabriela.local>
2023-05-02 17:38:58 +0200

    Changed pizzeria model tree

--- mongoDB_structures/pizzeria.js
+++ mongoDB_structures/pizzeria.js
@@ -1,66 +1,73 @@
 use pizzeriaShop
 
-db.createCollection("pizzeria")
-db.createCollection("order")
-db.createCollection("client")
-db.createCollection("product")
+db.CreateCollection("client")
+db.CreateCollection("pizzeria")
+db.CreateCollection("employee")
+db.CreateCollection("category")
+db.CreateCollection("product")
+db.CreateCollection("order")
 
-db.pizzeria.insertOne({
-    id: 1,
-    name: "Pizzeria X",
-    address: [{
-        street: "Rafael Casanova, 6", 
-        floor: 1, 
-        city: "Barcelona", 
-        zipcode: 08186, 
-        country: "Spain"
-    }],
-    employee: [{
-        id: 1,
+db.client.insertOne
+({
+        name: "Client X",
+        lastName: "kjh",
+        address: [{
+          street: "La font de sant cristofol, 3",
+          zipCode: 08187,
+          city: "Barcelona",
+          country: "Spain"
+        }],
+        phone: 123456789
+})
+
+db.pizzeria.insertOne
+({
+        address: [{
+          street: "Rafael Casanova, 6",
+          zipcode: 08186,
+          city: "Barcelona",
+          country: "Spain"
+        }]
+      
+})
+
+db.employee.insertOne
+({
         name: "Paolo",
-        nif: "111111S",
+        lastName: "Esposito",
+        nif: "11111111S",
         phone: 222222227,
-        type: [{
-            job: "deliveryMan"
-         }]
-    }]
-})
-db.order.insertOne({
-    id: 1,
-    dateTime: Date(),
-    delivery: [{
-        type: "delivery"
-    }],
-    detailOrder: [{
-        id: 1,
-        productID: 1,
-        quantity: 4,
-    }],
-    totalProducts: 4,
-    totalPrice: 143.96,
-    clientID: 1,
-    pizzeriaID: 1,
-    employeeID: 1
+        type: "deliveryMan",
+        pizzeriaId: ObjectId("6153d5ab7e6932a0d3c73b77")
+      
 })
 
-db.client.insertOne({
-    id: 1,
-    name: "Client X",
-    lastName: "kjhb",
-    phone: 123457689,
-    address: [{
-        street: "La Font de Sant Cristofol, 3", 
-        floor: 1, 
-        city: "Barcelona", 
-        zipcode: 08187, 
-        country: "Spain"
-     }]
+db.category.insertOne
+({
+        name: "Classics"
+      
 })
-db.product.insertOne({
-    id: 1,
-    category: "Spicy",
-    name: "Diavolo",
-    description: "Pizza with spicy ingredients",
-    image: File(),
-    price: 35,99
+
+db.product.insertOne
+({
+        name: "Diavolo Pizza",
+        description: "Spicy ingredients",
+        image: File(),
+        price: 12.50,
+        categoryId: ObjectId("615a6e5768d524c6a07ee2e7")
 })
+
+db.order.insertOne
+({
+        DateTime: Date(),
+        deliveryType: "Delivery",
+        clientId: ObjectId("615a6ca768d524c6a07ee2e4"),
+        employeeId: ObjectId("615a6f4168d524c6a07ee2e9"),
+        pizzeriaId: ObjectId("615a6d0768d524c6a07ee2e5"),
+        products: [{
+            productId: ObjectId("615a709d68d524c6a07ee2ea"),
+            quantity: 2
+        }],
+        totalPrice: 25
+})
+  
